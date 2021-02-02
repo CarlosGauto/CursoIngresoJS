1137ad1626bbbe1899084e98cca7c89ec0144108
Carlos Gauto <rca.gauto@gmail.com>
2021-02-01 22:57:30 -0300

    Ejercicio IF 5 Terminado

--- 2-InstruccionIf/jsInstruccionIF-05.js
+++ 2-InstruccionIf/jsInstruccionIF-05.js
@@ -1,6 +1,13 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = parseInt(txtIdEdad.value);
+
+	if (edad >= 18) {
+		alert("Usted es no es un adolescente ");
+	} else if (edad < 13) {
+		alert("Usted es no es un adolescente ");
+	} 
 
 }//FIN DE LA FUNCIÓN
