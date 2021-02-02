9fbf8b75f65e51b2f02aad5aafcec74fa3117360
Carlos Gauto <rca.gauto@gmail.com>
2021-02-01 21:50:50 -0300

    Ejercicio IF 4 Terminado

--- 2-InstruccionIf/jsInstruccionIF-04.js
+++ 2-InstruccionIf/jsInstruccionIF-04.js
@@ -1,7 +1,14 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
+	edad = parseInt(txtIdEdad.value);
+
+	if (edad >= 18 ) {
+		
+	} else if (edad >= 13){
+	
+	alert("Usted es un adolecente");
+	}
 
 }//FIN DE LA FUNCIÓN
