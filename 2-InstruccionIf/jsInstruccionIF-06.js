a3604a15de3457e8789e893b5b7d9d197cf44ef9
Carlos Gauto <rca.gauto@gmail.com>
2021-02-02 19:09:53 -0300

    Ejercicio IF 06 terminado

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,17 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
 
+	edad = parseInt(txtIdEdad.value);
+
+	if (edad >= 18) {
+		alert ("Sos mayor de edad");
+
+	}else if (edad >= 13){
+		alert ("Sos un adolescente");
+	} else 
+		alert ( "Sos un niño");
+	
 
 
 }//FIN DE LA FUNCIÓN
