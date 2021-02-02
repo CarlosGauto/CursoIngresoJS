9362087aa81565ccdf7e8e42588b7d36129de35e
Carlos Gauto <rca.gauto@gmail.com>
2021-02-01 21:34:09 -0300

    Ejercicio 2 terminado

--- 2-InstruccionIf/jsInstruccionIF-02.js
+++ 2-InstruccionIf/jsInstruccionIF-02.js
@@ -1,7 +1,11 @@
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+
+	edad = parseInt(txtIdEdad.value);
 	
+	if (edad >= 18) {
+		alert ("Sos mayor de edad");
+	}
 
 }//FIN DE LA FUNCIÓN
