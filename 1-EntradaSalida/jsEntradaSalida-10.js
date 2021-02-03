f063dd82660be89bf3196fba6714e3efa3bbadef
Carlos Gauto <rca.gauto@gmail.com>
2021-02-02 22:30:39 -0300

    Entrega en clase

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -6,11 +6,19 @@ en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
 	let importe;
+	let resultado;
 	let descuento;
 
 	importe = txtIdImporte.value;
 	importe = parseInt(importe);
-	descuento = importe - (importe * 0.25) ;
-	txtIdResultado.value = descuento;
+
+	
+	//descuento = importe * 25/100;
+	//resultado = importe - (importe * 0.25);
+
+	descuento = importe * 0.25;
+
+	resultado = importe - descuento ;
+	txtIdResultado.value = resultado;
 
 }
