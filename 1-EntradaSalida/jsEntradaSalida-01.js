1fb4283824593d93bde277a8fa9d74fd5bdac426
Carlos Gauto <rca.gauto@gmail.com>
2021-02-18 20:23:08 -0300

    .

--- 1-EntradaSalida/jsEntradaSalida-01.js
+++ 1-EntradaSalida/jsEntradaSalida-01.js
@@ -3,6 +3,43 @@ Al presionar el  botón,
 se debe mostrar un mensaje como el siguiente "Esto funciona de maravilla"*/
 function mostrar()
 {
-	alert("Esto funciona de maravilla");
+	let num1;
+   let num2;
+   let num3;
+   let num4;
+   let num5;
+   let num6;
+   let num7;
+   let num8;
+   let num9;
+   let num10;
+   
+   let suma;
+   
+   
+   num1 = prompt("Ingrese el primer numero para sumar");
+   num1 = parseInt(num1);
+   num2 = prompt("Ingrese el segundo numero para sumar");
+   num2 = parseInt(num2);
+   num3 = prompt("Ingrese el tercer numero para sumar");
+   num3 = parseInt(num3);
+   num4 = prompt("Ingrese el primer numero para sumar");
+   num4 = parseInt(num4);
+   num5 = prompt("Ingrese el segundo numero para sumar");
+   num5 = parseInt(num5);
+   num6 = prompt("Ingrese el tercer numero para sumar");
+   num6 = parseInt(num6);
+   num7 = prompt("Ingrese el primer numero para sumar");
+   num7 = parseInt(num7);
+   num8 = prompt("Ingrese el segundo numero para sumar");
+   num8 = parseInt(num8);
+   num9 = prompt("Ingrese el tercer numero para sumar");
+   num9 = parseInt(num9);
+   num10 = prompt("Ingrese el primer numero para sumar");
+   num10 = parseInt(num10);
+   
+   suma = num1 + num2 + num3 + num4 + num5 + num6 + num7 + num8 + num9 + num10;
+   
+   alert( "La suma de los 3 numeros ingresados es " + suma);
 }
 
