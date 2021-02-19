b9ea6f8e71395f30fc0feb6fb2105689a26632f4
Carlos Gauto <rca.gauto@gmail.com>
2021-02-18 21:52:03 -0300

    Ejercicios While

--- 5-InstruccionWhile/jsIteracionesWhile-01.js
+++ 5-InstruccionWhile/jsIteracionesWhile-01.js
@@ -3,5 +3,13 @@ al presionar el botón mostrar 10 repeticiones
 con números ASCENDENTE, desde el 1 al 10.*/
 function mostrar()
 {
-	alert('iteración while');
+	let i;
+	i = 1;
+
+	while ( i < 11 )
+	{
+		console.log (i);
+		i = i + 1;
+
+	}
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteracionesWhile-03.js
+++ 5-InstruccionWhile/jsIteracionesWhile-03.js
@@ -2,7 +2,20 @@
 al presionar el botón pedir la CLAVE (ayuda: es utn750)*/
 function mostrar()
 {
-	var claveIngresada;
-	claveIngresada = prompt("ingrese el número clave.");
+	let claveIngresada;
+	let intentos;
+
+	intentos = 0;
+	claveIngresada = prompt( "Ingrese el numero clave" );
+
 	
+	while (claveIngresada != "utn750" && intentos < 2)
+	{
+		claveIngresada = prompt("Clave erronea, ingrese el número clave correcto");
+		intentos = intentos + 1;
+	}
+	
+	
+	
+
 }//FIN DE LA FUNCIÓN
